83267996a62b5cae3e05ae1659ca73ebc8500696
Cameron Dunn <30559428+cdunn2013@users.noreply.github.com>
2019-02-04 17:26:53 -0600

    Beginning of app with sign up page finished.

---
+++ EZTip/EZTip/View Controllers/ProfileCompletionViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ProfileCompletionViewController.swift
+//  EZTip
+//
+//  Created by Cameron Dunn on 2/4/19.
+//  Copyright © 2019 Cameron Dunn. All rights reserved.
+//
+
+import UIKit
+
+class ProfileCompletionViewController: UIViewController {
+
+    @IBOutlet weak var checkmark: UIImageView!
+    @IBOutlet weak var checkmarkBottomConstraint: NSLayoutConstraint!
+    var checkmarkCenter : CGPoint = CGPoint()
+    @IBOutlet weak var greenCircle: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        checkmarkCenter = checkmark.center
+        checkmarkBottomConstraint.constant = -400
+        greenCircle.transform = CGAffineTransform(scaleX: 0, y: 0)
+        
+        // Do any additional setup after loading the view.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.animateKeyframes(withDuration: 1, delay: 0, options: [], animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.7, animations: {
+                self.checkmark.center = self.checkmarkCenter
+            })
+            UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0, options: [], animations: {
+                self.greenCircle.transform = .identity
+            }, completion: nil)
+        }, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ EZTip/EZTip/View Controllers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  EZTip
+//
+//  Created by Cameron Dunn on 2/4/19.
+//  Copyright © 2019 Cameron Dunn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.navigationController?.isNavigationBarHidden = true
+    }
+
+
+}
+
